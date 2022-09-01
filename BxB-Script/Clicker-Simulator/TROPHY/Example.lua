1449585049df7766fd274fce4a5e71a9a659b906
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-01 12:09:48 +0700

    Create Example.lua

---
+++ BxB-Script/Clicker-Simulator/TROPHY/Example.lua
@@ -0,0 +1,33 @@
+
+local WallyV2 = select(2, pcall(function()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script/Clicker-Simulator/TROPHY/TPC-Lib.lua", true))()
+end))
+
+if type(WallyV2) == 'table' then
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+
+    local Window = WallyV2:CreateWindow("Clicker Simulator")
+    Window:Section("Trophies")
+    Window:Button("Collect Trophies", function()
+        local Head = select(2, pcall(function()
+            return LocalPlayer.Character.Head
+        end))
+        local Drops = select(2, pcall(function()
+            return workspace.Zones.HalfYear.Drops
+        end))
+        if typeof(Head) == 'Instance' and Head:IsA("BasePart") and typeof(Drops) == 'Instance' then
+            for _, v in pairs(Drops:GetDescendants()) do
+                if v:IsA("TouchTransmitter") then
+                    local Part = v.Parent
+                    if typeof(Part) == 'Instance' and Part:IsA("BasePart") then
+                        firetouchinterest(Head, Part, 0)
+                        task.wait(.01)
+                        firetouchinterest(Head, Part, 1)
+                    end
+                end
+            end
+        end
+    end)
+end
+end
