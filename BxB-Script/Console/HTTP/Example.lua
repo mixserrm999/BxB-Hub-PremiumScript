da7d79500ada0da268b918b6821a9ef23f575011
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-07 21:25:16 +0700

    Create Example.lua

---
+++ BxB-Script/Console/HTTP/Example.lua
@@ -0,0 +1,15 @@
+local HttpSpy = loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script/Console/HTTP/console-httpspy-save.lua"))({
+    AutoDecode = true, -- Automatically decodes JSON
+    Highlighting = true, -- Highlights the output
+    SaveLogs = true, -- Save logs to a text file
+    CLICommands = true, -- Allows you to input commands into the console
+    ShowResponse = true, -- Shows the request response
+    BlockedURLs = {} -- Blocked urls
+});
+
+HttpSpy:HookSynRequest("https://httpbin.org/get", function(response) 
+    response.Body = "hi";
+    return response;
+end);
+
+print(syn.request({ Url = "https://httpbin.org/get" }).Body);
