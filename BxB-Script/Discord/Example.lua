ebf3fe9bef6cb9854cee17b106c97891bf801121
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-11 20:21:08 +0700

    Delete Example.lua

--- BxB-Script/Discord/Example.lua
+++
@@ -1,9 +0,0 @@
-
-rconsolename("BxB Hub | Anti-Discord-Invites")
-assert(loadstring,"UNSUPPORTED EXPLOIT")
-loadstring(syn.request({
-    ["Url"] = "https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script/Discord/Anti-Discord-inv.lua",
-    ["Method"] = "GET"
-    })
-    ["Body"])()
-rconsoleinput()
