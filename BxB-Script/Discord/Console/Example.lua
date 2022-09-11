d133a1750a44ac28a3b02c7846a87b6ffde3145c
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-11 20:21:18 +0700

    Create Example.lua

---
+++ BxB-Script/Discord/Console/Example.lua
@@ -0,0 +1,9 @@
+
+rconsolename("BxB Hub | Anti-Discord-Invites")
+assert(loadstring,"UNSUPPORTED EXPLOIT")
+loadstring(syn.request({
+    ["Url"] = "https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script/Discord/Anti-Discord-inv.lua",
+    ["Method"] = "GET"
+    })
+    ["Body"])()
+rconsoleinput()
