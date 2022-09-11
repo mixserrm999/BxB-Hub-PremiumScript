97323267a06f17ea37cfef46e49d5455af3fb840
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-11 20:35:46 +0700

    Create Example.lua

---
+++ BxB-Script/Anti-DisplayName/Example.lua
@@ -0,0 +1,22 @@
+local AntiDisplayName = loadstring(game:HttpGet('https://pastebin.com/raw/hCMXnKWa'))(function()
+    getgenv().Preferences = {}
+end)
+
+Preferences = {
+    RetroNaming = false,
+    ShowOriginalName = true,
+    ApplyToLeaderboard = true,
+    IdentifyFriends = {Toggle = true, Identifier = '[Friend]'},
+    IdentifyBlocked = {Toggle = true, Identifier = '[Blocked]'},
+    IdentifyPremium = {Toggle = true, Identifier = '[Premium]'},
+    IdentifyDeveloper = {Toggle = true, Identifier = '[Game Dev]'},
+    SpoofLocalPlayer = {Toggle = false, UseRandomName = true, NewName = 'Random Name Lol'},
+    Orientation = 'Horizontal'
+}
+
+--[[
+    -- Terms of Service
+     - https://v3rmillion.net/member.php?action...e&uid=2988.
+     - Don't sell this script.
+     - Don't steal credit for this script.
+]]
