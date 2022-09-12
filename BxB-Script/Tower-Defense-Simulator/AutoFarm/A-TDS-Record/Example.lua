ba925ba25b40fa3dd4b6204d650447f81ddd3f6a
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-09-12 23:03:29 +0700

    Create Example.lua

---
+++ BxB-Script/Tower-Defense-Simulator/AutoFarm/A-TDS-Record/Example.lua
@@ -0,0 +1,5 @@
+getgenv().StratName = "Recorder"
+getgenv().APIVar = "TDS"
+getgenv().Webhook = "WEBHOOK (OPTIONAL)"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script/Tower-Defense-Simulator/AutoFarm/A-TDS-Record/Record-API.lua"))()
