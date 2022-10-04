8182f46260beb8c73a5b2759d5f8587c2c8df35a
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-10-04 12:38:52 +0700

    Update SaveSettingAPI.lua

--- BxB-Save-Settings/SaveSettingAPI.lua
+++ BxB-Save-Settings/SaveSettingAPI.lua
@@ -1,4 +1,5 @@
-getgenv()['MyName'] = game.Players.LocalPlayer.Name
+getgenv()['P_Name'] = game.Players.LocalPlayer.Name
+getgenv()['G_Name'] = game.PlaceId
 getgenv()['JsonEncode'] = function(msg)
     return game:GetService("HttpService"):JSONEncode(msg)
 end
@@ -6,24 +7,24 @@ getgenv()['JsonDecode'] = function(msg)
     return game:GetService("HttpService"):JSONDecode(msg)
 end
 getgenv()['Check_Setting'] = function(Name)
-    if not isfolder('SaveSetting') then
-        makefolder('SaveSetting')
+    if not isfolder('BxB_Save_Setting') then
+        makefolder('BxB_Save_Setting')
     end
-    if not isfile('SaveSetting/'..Name..'_'..'Setting.json') then
-        writefile('SaveSetting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
+    if not isfile('BxB_Save_Setting/'..Name..'_'..'Setting.json') then
+        writefile('BxB_Save_Setting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
     end
 end
 getgenv()['Get_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'Setting.json') then
-        getgenv().Setting = JsonDecode(readfile('SaveSetting/'..Name..'_'..'Setting.json'))
+    if isfolder('BxB_Save_Setting') and isfile('BxB_Save_Setting/'..Name..'_'..'Setting.json') then
+        getgenv().Setting = JsonDecode(readfile('BxB_Save_Setting/'..Name..'_'..'Setting.json'))
         return getgenv().Setting
     else
         Check_Setting(Name)
     end
 end
 getgenv()['Update_Setting'] = function(Name)
-    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'Setting.json') then
-        writefile('SaveSetting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
+    if isfolder('BxB_Save_Setting') and isfile('BxB_Save_Setting/'..Name..'_'..'Setting.json') then
+        writefile('BxB_Save_Setting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
     else
         Check_Setting(Name)
     end
