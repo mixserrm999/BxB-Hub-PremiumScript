ef7329726f36678d6b8ae0f9a51e89da15d36d78
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-10-04 12:29:19 +0700

    Update SaveSettingAPI.lua

--- BxB-Save-Settings/SaveSettingAPI.lua
+++ BxB-Save-Settings/SaveSettingAPI.lua
@@ -6,24 +6,24 @@ getgenv()['JsonDecode'] = function(msg)
     return game:GetService("HttpService"):JSONDecode(msg)
 end
 getgenv()['Check_Setting'] = function(Name)
-    if not isfolder('BxB_Save_Settings') then
-        makefolder('BxB_Save_Settings')
+    if not isfolder('SaveSetting') then
+        makefolder('SaveSetting')
     end
-    if not isfile('BxB_Save_Settings/'..Name..'_'..'Setting.json') then
-        writefile('BxB_Save_Settings/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
+    if not isfile('SaveSetting/'..Name..'_'..'Setting.json') then
+        writefile('SaveSetting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
     end
 end
 getgenv()['Get_Setting'] = function(Name)
-    if isfolder('BxB_Save_Settings') and isfile('BxB_Save_Settings/'..Name..'_'..'Setting.json') then
-        getgenv().Setting = JsonDecode(readfile('BxB_Save_Settings/'..Name..'_'..'Setting.json'))
+    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'Setting.json') then
+        getgenv().Setting = JsonDecode(readfile('SaveSetting/'..Name..'_'..'Setting.json'))
         return getgenv().Setting
     else
         Check_Setting(Name)
     end
 end
 getgenv()['Update_Setting'] = function(Name)
-    if isfolder('BxB_Save_Settings') and isfile('BxB_Save_Settings/'..Name..'_'..'Setting.json') then
-        writefile('BxB_Save_Settings/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
+    if isfolder('SaveSetting') and isfile('SaveSetting/'..Name..'_'..'Setting.json') then
+        writefile('SaveSetting/'..Name..'_'..'Setting.json',JsonEncode(getgenv().Setting))
     else
         Check_Setting(Name)
     end
