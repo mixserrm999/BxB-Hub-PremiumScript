90cfdb96b52a8dc7eddfae6d9ef02a777884b4ee
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-10-15 21:06:43 +0700

    Update HWID-Whitelist.lua

--- Whitelist/HWID-Whitelist.lua
+++ Whitelist/HWID-Whitelist.lua
@@ -18,3 +18,51 @@ end
         print("Not Have Key")
 setclipboard("Key:"..(Key).."  |  "..(ClientId))
 end
+
+--// UPDATE 15/10/22
+
+_G.WHITELIST = "ZED_Orian" 
+
+
+print("[1] Check Key...")
+--// Local Data Key + Hwid
+local Hwid = {
+   ["ZED_Orian"] = "FF0272E9-3286-4B88-AB02-C722A4B1618E",
+   ["ADD"] = "ADD"
+   --["KEY"] = true,
+--loadstring(game:HttpGetAsync("SCRIPT", true))();
+}
+
+--// END Data Key + Hwid
+print("[2] Check Hwid...")
+--// Local Whitelist
+local Player = game.Players.LocalPlayer.Name
+local ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
+local Hwide = string.split((ClientId),'-')
+local Name = Player--Hwide[1]
+local keywhitelist = game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/Key_Whitelist.lua", true)
+print("[3] Check Whitelist...")
+
+--// Check Key [1/2]
+if _G.WHITELIST == Name then
+    --// Check HIWD [2/2]
+if Hwid[_G.WHITELIST] == game:GetService("RbxAnalyticsService"):GetClientId() then
+
+print("Access..! | Script Loading..." )
+--// Script
+
+
+-- End Script
+else
+   warn("Not Have Whitelist [ Hwid Exploit ]")
+ --setclipboard("Key: "..(Name).." Hwid "..(ClientId))
+end
+
+    --// END HIWD [2/2]
+else
+    warn("[ * ] Not Have Whitelist [ Player Name ]")
+--setclipboard("Key: "..(Name).." Hwid "..(ClientId))
+    
+end
+
+--// END KEY [1/2]
