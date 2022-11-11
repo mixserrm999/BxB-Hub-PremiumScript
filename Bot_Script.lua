4fee876aaa20441008a58efa97c57e2517d9377a
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-11 20:32:03 +0700

    Create Bot_Script.lua

---
+++ Bot_Script.lua
@@ -0,0 +1,35 @@
+local OWNER = "https://raw.githubusercontent.com"
+local DATA = "/BxB-HUB/BxB-Hub-PremiumScript/main/BxB-Script"
+local Games = {
+    [2753915549] = "/Blox-Fruit/Main/Script/Main.lua",
+    [4442272183] = "/Blox-Fruit/Main/Script/Main.lua",
+    [7449423635] = "/Blox-Fruit/Main/Script/Main.lua",
+    [292439477] = "/Phantom-Forces/Main/BxB-Hub-PF-V2.lua",
+    [286090429] = "/Arsenal/Main/Arsenal.lua",
+    [9648883891] = "/Festival-Tycoon/UI-Script/FTT.lua",
+    [8304191830] = "/Anime-Adventures/AAT.lua",
+    [9284097280] = "/Apartment-Tycoon/APTC.lua",
+    [4490140733] = "/My-Restaurant/MR.lua",
+    [8737602449] = "/PLS-Donate/PLSDONATE.lua",
+    [3956818381] = "/Ninja-Legends/NL.lua",
+    [6284583030] = "/Pet-Simulator-X/BxB-Meow/Example.lua"
+}
+--// LoadScript
+local Supported = Games[game.PlaceId] or false
+if Supported ~= false then
+    loadstring(game:HttpGet(OWNER..""..DATA..""..Supported))()
+end
+--// Notify
+
+if not game.PlaceId == Supported then
+    local hint = Instance.new("Hint", game.CoreGui)
+    local req = (syn and syn.request) or (http and http.request) or http_request or request
+    warn("Not a supported game")
+    hint.Text = "This game is not supported"
+    delay(
+        5,
+        function()
+            hint:destroy()
+        end
+    )
+end
