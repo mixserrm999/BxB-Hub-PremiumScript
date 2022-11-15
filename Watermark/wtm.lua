cbf81c2f53c093475757afb5e33841ae5c414e35
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-15 17:40:55 +0700

    Create wtm.lua

---
+++ Watermark/wtm.lua
@@ -0,0 +1,6 @@
+repeat wait() until game:IsLoaded()
+
+library = loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/Watermark/wtm.lib.lua"))()
+
+local watermark = library:Watermark("BxB.xyz | Premium | Community | "..os.date())
+
