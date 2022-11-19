3becd862673e9b809341d3dbbd5e0cabbc153b46
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-19 12:29:49 +0700

    Create Example.lua

---
+++ OBF/BxB_Pro_OBF/Example.lua
@@ -0,0 +1,7 @@
+getgenv().customVar = "Duckyy"
+getgenv().watermark = "Secure by kosuke14"
+getgenv().source = [[
+
+]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/OBF/BxB_Pro_OBF/Part.lua"))()
