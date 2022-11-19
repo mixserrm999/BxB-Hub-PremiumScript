7429da2a1c7c36f11398c7211b67307d8738ef39
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-19 12:01:16 +0700

    Create Config_Save.lua

---
+++ BxB-Save-Settings/Config_Save.lua
@@ -0,0 +1,240 @@
+
+_G.Settings = {
+
+	Main = {
+		["Auto Farm Level"] = false,
+		["Fast Auto Farm Level"] = false,
+
+		--[Mob Aura]
+
+		["Distance Mob Aura"] = 1000, -- {Max : 5000} 
+		["Mob Aura"] = false,
+
+		--[World 1]
+		["Auto New World"] = false,
+		["Auto Saber"] = false,
+		["Auto Pole"] = false,
+
+		["Auto Buy Ablility"] = false,
+
+		--[World 2]
+		["Auto Third Sea"] = false,
+		["Auto Factory"] = false,
+		["Auto Factory Hop"] = false,
+		["Auto Bartilo Quest"] = false,
+
+		["Auto True Triple Katana"] = false,
+		["Auto Rengoku"] = false,
+		["Auto Swan Glasses"] = false,
+		["Auto Dark Coat"] = false,
+		["Auto Ectoplasm"] = false,
+
+		["Auto Buy Legendary Sword"] = false,
+		["Auto Buy Enchanment Haki"] = false,
+
+		--[World 3]
+		["Auto Holy Torch"] = false,
+		["Auto Buddy Swords"] = false,
+		["Auto Farm Boss Hallow"] = false,
+		["Auto Rainbow Haki"] = false,
+		["Auto Elite Hunter"] = false,
+		["Auto Musketeer Hat"] = false,
+		["Auto Buddy Sword"] = false,
+		["Auto Farm Bone"] = false,
+		["Auto Ken-Haki V2"] = false,
+		["Auto Cavander"] = false,
+		["Auto Yama Sword"] = false,
+		["Auto Tushita Sword"] = false,
+		["Auto Serpent Bow"] = false,
+		["Auto Dark Dagger"] = false,
+		["Auto Cake Prince"] = false,
+		["Auto Dough V2"] = false,
+		["Auto Random Bone"] = false,
+
+		--[For God Human]
+
+		["Auto Fish Tail Sea 1"] = false,
+		["Auto Fish Tail Sea 3"] = false,
+		["Auto Magma Ore Sea 2"] = false,
+		["Auto Magma Ore Sea 1"] = false,
+		["Auto Mystic Droplet"] = false,
+		["Auto Dragon Scales"] = false,
+
+	},
+	FightingStyle = {
+		["Auto God Human"] = false,
+		["Auto Superhuman"] = false,
+		["Auto Electric Claw"] = false,
+		["Auto Death Step"] = false,
+		["Auto Fully Death Step"] = false,
+		["Auto SharkMan Karate"] = false,
+		["Auto Fully SharkMan Karate"] = false,
+		["Auto Dragon Talon"] = false,
+	},
+	Boss = {
+		["Auto All Boss"] = false,
+		["Auto Boss Select"] = false,
+		["Select Boss"] = {},
+
+		["Auto Quest"] = false,
+	},
+	Mastery = {
+		["Select Multi Sword"] = {},
+		["Farm Mastery SwordList"] = false,
+		["Auto Farm Fruit Mastery"] = false,
+		["Auto Farm Gun Mastery"] = false,
+		["Mob Health (%)"] = 15,
+	},
+	Configs = {
+		["Double Quest"] = false,
+		["Bypass TP"] = false,
+		["Select Team"] = {"Pirate"}, --{Pirate,Marine}
+
+
+		["Fast Attack"] = true,
+		["Fast Attack Type"] = {"Fast"}, --{Normal,Fast,Slow}
+
+		["Select Weapon"] = {},
+
+
+		--[Misc Configs]
+		["Auto Haki"] = true,
+		["Distance Auto Farm"] = 20, --{Max : 50}
+		["Camera Shaker"] = false,
+
+		--[Skill Configs]
+		["Skill Z"] = true,
+		["Skill X"] = true,
+		["Skill C"] = true,
+		["Skill V"] = true,
+
+		--[Mob Configs]
+		["Show Hitbox"] = false,
+		["Bring Mob"] = true,
+		["Disabled Damage"] = false,
+
+	},
+	Stat = {
+		--[Auto Stats]
+		["Enabled Auto Stats"] = false,
+		["Auto Stats Kaitun"] = false,
+
+		["Select Stats"] = {"Melee"}, --{Max Stats,Melee,Defense,Sword,Devil Fruit,Gun}
+		["Point Select"] = 3, --{Recommended , Max : 9}
+
+		--[Auto Redeem Code]
+
+		["Enabled Auto Redeem Code"] = false,
+		["Select Level Redeem Code"] = 1, --{Max : 2400}
+	},
+
+	Misc = {
+		["No Soru Cooldown"] = false,
+		["No Dash Cooldown"] = false,
+
+		["Infinities Geppo"] = false,
+		["Infinities Energy"] = false,
+
+		["No Fog"] = false,
+		["Wall-TP"] = false,
+
+		["Fly"] = false,
+		["Fly Speed"] = 1,
+
+		--[Server]
+		["Auto Rejoin"] = true,
+	},
+	Teleport = {
+		["Teleport to Sea Beast"] = false,
+	},
+
+	Fruits = {
+		["Auto Buy Random Fruits"] = false,
+		["Auto Store Fruits"] = false,
+
+		["Select Devil Fruits"] = {}, -- {"Bomb-Bomb","Spike-Spike","Chop-Chop","Spring-Spring","Kilo-Kilo","Spin-Spin","Kilo-Kilo","Spin-Spin","Bird: Falcon","Smoke-Smoke","Flame-Flame","Ice-Ice","Sand-Sand","Dark-Dark","Revive-Revive","Diamond-Diamond","Light-Light","Love-Love","Rubber-Rubber","Barrier-Barrier","Magma-Magma","Door-Door","Quake-Quake","Human-Human: Buddha","String-String","Bird-Bird: Phoenix","Rumble-Rumble","Paw-Paw","Gravity-Gravity","Dough-Dough","Shadow-Shadow","Venom-Venom","Control-Control","Soul-Soul","Dragon-Dragon"}
+		["Auto Buy Devil Fruits Sniper"] = false,
+	},
+
+	Raids = {
+		["Auto Raids"] = false,
+
+		["Kill Aura"] = false,
+		["Auto Awakened"] = false,
+		["Auto Next Place"] = false,
+
+		["Select Raids"] = {}, -- {"Flame","Ice","Quake","Light","Dark","String","Rumble","Magma","Human: Buddha","Sand","Bird: Phoenix","Dough"},
+	},
+
+	Combat = {
+		["Fov Size"] = 200,
+		["Show Fov"] = false,
+		["Aimbot Skill"] = false,
+	},
+
+	HUD = {
+		["FPS"] = 60,
+		["LockFPS"] = true,
+		["Boost FPS Windows"] = false,
+		['White Screen'] = false,
+	},
+	ConfigsUI = {
+		ColorUI = Color3.fromRGB(255, 0, 127), --{Color UI}
+	}
+}
+
+_G.Kai = {
+	["Check Swords"] = {
+		["Enabled Check"] = true,
+	},
+	["Check Fighting Style"] = {
+		["Enabled Check"] = true,
+	},
+	["Check Awakening Fruits"] = {
+		["Enabled Check"] = true,
+	},
+	["Check Fruits"] = {
+		["Enabled Check"] = true,
+	},
+}
+
+function LoadSettings()
+	if readfile and writefile and isfile and isfolder then
+		if not isfolder("BxB Hub Premium Scripts") then
+			makefolder("BxB Hub Premium Scripts")
+		end
+		if not isfolder("BxB Hub Premium Scripts/Blox Fruits/") then
+			makefolder("BxB Hub Premium Scripts/Blox Fruits/")
+		end
+		if not isfile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json") then
+			writefile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(_G.Settings))
+		else
+			local Decode = game:GetService("HttpService"):JSONDecode(readfile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"))
+			for i,v in pairs(Decode) do
+				_G.Settings[i] = v
+			end
+		end
+	else
+		return warn("Status : Undetected Executor")
+	end
+end
+
+function SaveSettings()
+	if readfile and writefile and isfile and isfolder then
+		if not isfile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json") then
+			LoadSettings()
+		else
+			local Decode = game:GetService("HttpService"):JSONDecode(readfile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json"))
+			local Array = {}
+			for i,v in pairs(_G.Settings) do
+				Array[i] = v
+			end
+			writefile("BxB Hub Premium Scripts/Blox Fruits/" .. game.Players.LocalPlayer.Name .. ".json", game:GetService("HttpService"):JSONEncode(Array))
+		end
+	else
+		return warn("Status : Undetected Executor")
+	end
+end
+
+LoadSettings()
+SaveSettings()
