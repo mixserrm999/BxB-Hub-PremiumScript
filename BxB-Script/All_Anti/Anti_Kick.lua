7339f8e138670eea0c8f9020c7991d1c180c8911
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-21 12:00:57 +0700

    Create Anti_Kick.lua

---
+++ BxB-Script/All_Anti/Anti_Kick.lua
@@ -0,0 +1,31 @@
+                local hook = game:GetService("Players").LocalPlayer.Kick;
+                local mt = getrawmetatable(game:GetService("Players").LocalPlayer)
+                local psuedoEnv = {
+                    ["__index"] = mt.__index,
+                    ["__namecall"] = mt.__namecall
+                }
+                local ReturnFunc = function()end
+                        
+                print("B-O-O-M#4134 -  AntiKick") -- remove this if you want, just dont claim it as yours ty
+                setreadonly(mt, false)
+                mt.__namecall = newcclosure(function(self, Index, ...)
+                    local NameCallMethod = getnamecallmethod()
+                    if type(NameCallMethod) == "string" then
+                        if NameCallMethod == "Kick" or NameCallMethod == "kick" then
+                            print("Prevented Kick At " .. tostring(os.date()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__namecall(self, Index, ...)
+                end)
+                        
+                mt.__index = newcclosure(function(self, Index, ...)
+                    if type(Index) == "string" then
+                        if Index == "Kick" or Index == "kick" then
+                            print("Prevented Kick At " .. tostring(os.time()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__index(self, Index, ...)
+                end)
+                setreadonly(mt, true)
