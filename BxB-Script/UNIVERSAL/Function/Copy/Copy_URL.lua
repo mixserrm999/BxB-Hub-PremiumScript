c520e50471faf9452628f70fe6fc4308dcce188b
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-21 12:18:21 +0700

    Create Copy_URL.lua

---
+++ BxB-Script/UNIVERSAL/Function/Copy/Copy_URL.lua
@@ -0,0 +1,3 @@
+local createAsset = loadstring(game:HttpGet("https://raw.githubusercontent.com/Skribb11es/Create-Asset-Function/main/createAsset.lua", true))()
+createAsset("Link url", "name", ".png/.lua/.txt/.json/.js/.py/.++++")
+
