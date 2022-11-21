4be9aabcf71807f5815c069ff51cc279e14031c3
BxB <101545758+BxB-HUB@users.noreply.github.com>
2022-11-21 12:01:26 +0700

    Create Anti_rejoin.lua

---
+++ BxB-Script/All_Anti/Anti_rejoin.lua
@@ -0,0 +1,13 @@
+_G.Rejoin = true
+spawn(function()
+    while true do wait()
+        getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(Kick)
+            if not _G.TP_Ser and _G.Rejoin then
+                if Kick.Name == 'ErrorPrompt' and Kick:FindFirstChild('MessageArea') and Kick.MessageArea:FindFirstChild("ErrorFrame") then
+                    game:GetService("TeleportService"):Teleport(game.PlaceId)
+                    wait(50)
+                end
+            end
+        end)
+    end
+end)
