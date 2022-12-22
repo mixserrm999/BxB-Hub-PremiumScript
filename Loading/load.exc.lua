d0a37253919eb50d1188ece007ffa87a1c23b475
BxB <101545758+BxB-xyz@users.noreply.github.com>
2022-12-22 20:13:59 +0700

    Update load.exc.lua

--- Loading/load.exc.lua
+++ Loading/load.exc.lua
@@ -5,23 +5,4 @@ shared.LoaderKeyFrames = {
 }
 loadstring(game:HttpGet'https://raw.githubusercontent.com/BxB-HUB/BxB-Hub-PremiumScript/main/Loading/ld.library.lua')()
 
-local Metadata = {
-	LoaderData = {
-		Name = (shared.LoaderTitle or 'A Loader'),
-		Colors = shared.LoaderColors or {
-			Main = Color3.fromRGB(24, 24, 24),
-			Topic = Color3.fromRGB(255, 255, 255),
-			Title = Color3.fromRGB(255, 255, 255),
-			LoaderBackground = Color3.fromRGB(30, 30, 30),
-			LoaderSplash = Color3.fromRGB(255, 255, 255)
-		}
-	},
-	Keyframes = shared.LoaderKeyFrames or {
-		[1] = {1, 10}, -- [Time (s), Percentage]
-		[2] = {2, 30},
-		[3] = {3, 60},
-		[4] = {2, 100}
-	}
-}
-
 print("SCS")
