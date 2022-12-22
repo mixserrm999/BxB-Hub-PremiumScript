9376b08c621f0b4c9d4335736a726552ae3634da
BxB <101545758+BxB-xyz@users.noreply.github.com>
2022-12-22 19:50:12 +0700

    Create No_Stun.lua

---
+++ BxB-Script/Blox-Fruit/Source_More/Function/No_Stun.lua
@@ -0,0 +1,13 @@
+                  -- [No Stun]
+
+task.spawn(function()
+	if game.Players.LocalPlayer.Character:FindFirstChild("Stun") then
+		game.Players.LocalPlayer.Character.Stun.Changed:connect(function()
+			pcall(function()
+				if game.Players.LocalPlayer.Character:FindFirstChild("Stun") then
+					game.Players.LocalPlayer.Character.Stun.Value = 0
+				end
+			end)
+		end)
+	end
+end)
