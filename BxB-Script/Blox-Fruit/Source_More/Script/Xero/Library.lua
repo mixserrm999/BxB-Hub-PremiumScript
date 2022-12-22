4e1644720b06e0448e65b175af716828abf6a2d7
BxB <101545758+BxB-xyz@users.noreply.github.com>
2022-12-22 19:45:45 +0700

    Update Library.lua

--- BxB-Script/Blox-Fruit/Source_More/Script/Xero/Library.lua
+++ BxB-Script/Blox-Fruit/Source_More/Script/Xero/Library.lua
@@ -29,10 +29,10 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
+    MainColor = Color3.fromRGB(0, 0, 0);
+    BackgroundColor = Color3.fromRGB(0, 0, 0);
+    AccentColor = Color3.fromRGB(255, 51, 51);
+    OutlineColor = Color3.fromRGB(0, 0, 0);
 
     Black = Color3.new(0, 0, 0);
 
